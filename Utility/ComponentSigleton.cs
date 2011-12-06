595c1264e2db7d4d4af4ed774cf797357f20ad6f
Tyler Steele <tyler.steele@mymail.champlain.edu>
2011-12-06 18:00:15 -0400

    first commit
    Signed-off-by: Tyler Steele <tyler.steele@mymail.champlain.edu>

---
+++ Utility/ComponentSigleton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ComponentSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get { return instance ?? (instance = new GameObject(typeof(T).ToString()).AddComponent<T>()); }
+    }
+
+    public virtual void Init()
+    {
+    }
+}
---
+++ Utility/Signleton.cs
@@ -0,0 +1,19 @@
+﻿// A generic singleton used in classes that are not components
+
+public class Singleton<T> where T : class, new()
+{
+    private static T instance;
+
+    public static T Instance
+    { 
+        get 
+        {
+            if (instance == null)
+            {
+                instance = new T();
+            }
+            return instance ;
+        }
+    }
+}
+
